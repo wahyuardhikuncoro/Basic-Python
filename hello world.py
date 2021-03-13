b4a2504c79a834de57e8cc4ff0e81b7cd9f03d0e
wahyuardhikuncoro <wahyuardhikuncoro@gmail.com>
2021-03-13 14:43:33 +0700

    hello world.py

---
+++ hello world.py
@@ -0,0 +1,23 @@
+#This is a comment
+
+"""Multi 
+Line
+comment"""
+
+print("Hello World")
+
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+a = 'text'
+b = True
+c = 10
+d = 1.1
+e = 1,1
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
